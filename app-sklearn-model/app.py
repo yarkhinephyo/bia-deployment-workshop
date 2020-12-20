9d6fba31910f626e9e1570189d90fe4448e3c601
yarkhinephyo <yarkhinephyo@gmail.com>
2020-12-20 11:29:06 +0800

    Add UI for app-sklearn

--- app-sklearn-model/app.py
+++ app-sklearn-model/app.py
@@ -1,18 +1,39 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+
 import pickle
 import sklearn
 
 app = Flask(__name__)
+CORS(app)
+
+model = pickle.load(open("simple-model.pkl", "rb"))
 
 @app.route('/')
-def hello_world():
+def index():
+  return render_template("index.html")
+
+@app.route('/predict')
+def predict():
   try:
-    model = pickle.load(open("simple-model.pkl", "rb"))
-    pred = model.predict([[5,4,3,1]])
-    print(pred)
-    return jsonify({"prediction": "hello world"})
+    args = request.args
+
+    sepal_length = float(args.get("sepalLength"))
+    sepal_width  = float(args.get("sepalWidth"))
+    petal_length = float(args.get("petalLength"))
+    petal_width  = float(args.get("petalWidth"))
+
+    pred = model.predict([[
+      sepal_length,
+      sepal_width,
+      petal_length,
+      petal_width
+      ]])
+
+    return jsonify({"message": str(pred[0])})
+
   except Exception as e:
-    return jsonify({"exception": str(e)})
+    return jsonify({"message": str(e)})
 
 if __name__ == "__main__":
   app.run()
