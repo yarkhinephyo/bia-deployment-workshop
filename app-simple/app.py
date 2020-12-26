fd131822c4b42d4aa60db760aa4e4714ed3591ba
yarkhinephyo <yarkhinephyo@gmail.com>
2020-12-26 14:29:56 +0800

    Change minor variable names

--- app-simple/app.py
+++ app-simple/app.py
@@ -4,9 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/hello')
-def hello():
-  return "Hello world"
+@app.route('/foo')
+def foo():
+  return "Foo"
 
 @app.route('/bia')
 def bia():
